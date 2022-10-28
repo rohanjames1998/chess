798c1ca29e9ecd1c839d482407de1ca348a350ae
Rohan James <rohanjames9282@gmail.com>
2022-10-28 14:39:15 +0530

    Add #make_chess_board

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,23 @@
 
 class Board
 
+  attr_accessor :grid
+
   def initialize
     @grid = {}
-    make_chess_board
   end
 
-  
+  def make_chess_board
+    dummy_ele = ''
+    col = []
+    row = 1
+    while row <= 8
+      while col.length < 8
+        col << dummy_ele
+      end
+      grid[row] = col
+      row += 1
+      col = []
+    end
+  end
+end
