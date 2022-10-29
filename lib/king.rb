3c8f2a9c24b92d49b71668d7eaeb4afe0cc54419
Rohan James <rohanjames9282@gmail.com>
2022-10-29 14:59:45 +0530

    Add king class

---
+++ lib/king.rb
@@ -0,0 +1,18 @@
+
+class King
+
+  attr_accessor :unicode
+
+  def initialize
+    @unicode = ''
+  end
+
+  def white
+    unicode = "\u2654"
+  end
+
+  def black
+    unicode = "\u265a"
+  end
+
+end
