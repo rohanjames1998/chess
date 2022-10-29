04ed9a26f1459003b197f699ce142d802588aa72
Rohan James <rohanjames9282@gmail.com>
2022-10-29 15:07:06 +0530

    Add knight class

---
+++ lib/knight.rb
@@ -0,0 +1,18 @@
+
+class Knight
+
+attr_accessor :unicode
+
+  def initialize
+    @unicode = ''
+  end
+
+  def white
+    unicode = "\u2658"
+  end
+
+  def black
+    unicode = "\u265e"
+  end
+
+end
