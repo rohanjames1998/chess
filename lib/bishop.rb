138c44a3b1468493e4d5489606c5ce007827f629
Rohan James <rohanjames9282@gmail.com>
2022-10-29 15:05:43 +0530

    Add bishop class

---
+++ lib/bishop.rb
@@ -0,0 +1,18 @@
+
+class Bishop
+
+  attr_accessor :unicode
+
+  def initialize
+    @unicode = ''
+  end
+
+  def white
+    unicode = "\u2657"
+  end
+
+  def black
+    unicode = "\u265d"
+  end
+
+end
