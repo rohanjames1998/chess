17b3526b80a290f261052b9b2696239e395d33db
Rohan James <rohanjames9282@gmail.com>
2022-10-29 15:01:28 +0530

    Add queen class

---
+++ lib/queen.rb
@@ -0,0 +1,18 @@
+
+class Queen
+
+  attr_accessor :unicode
+
+  def initialize
+    @unicode = ''
+  end
+
+  def white
+    unicode = "\u2655"
+  end
+
+  def black
+    unicode ="\u265b"
+  end
+
+end
