cc15563223174582665fe4d20bc19f785b809b2d
Rohan James <rohanjames9282@gmail.com>
2022-11-05 12:13:30 +0530

    Add #get_player_piece

--- lib/chess.rb
+++ lib/chess.rb
@@ -3,6 +3,7 @@ require_relative 'board'
 
 class Chess
 
+  attr_reader :board
 
   def initialize
     @p1 = Player.new
@@ -19,7 +20,35 @@ class Chess
     display_valid_input_format
     return false
   end
+
+  # This method prompts player to select a piece to move on the board. Then it proceeds to check if
+  # given input is valid or not.
+  def get_player_piece(player)
+    loop do
+      puts "Which piece do you want to move?:"
+      input = gets.chomp.delete(' ')
+      # This conditional checks whether the given input is valid && there is a piece present at the given
+      # location
+      if valid_location?(input) && board[input] != " "
+        chosen_piece = board[input]
+        player.chosen_piece = chosen_piece
+        get_piece_placement(chosen_piece)
+        break
+      else
+        next
+      end
+    end
+  end
+
+  def display_valid_input_format
+    puts "\n When choosing which piece to move or where to put said piece",
+         "the only valid input you can give has to be like:",
+         "1a",
+         "Where '1' is the row and 'a' is the column of the board.",
+         "Remember your row cannot be greater than 8 and your column cannot be any
+         alphabet higher than 'h'."
+  end
+
+  def get_piece_placement(chosen_piece)
+  end
 end
-
-
-
