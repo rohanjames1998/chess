b21bf2d1dd05a0cb5ddc8fb83425a268e91b1d1f
Rohan James <rohanjames9282@gmail.com>
2022-11-06 10:46:22 +0530

    Refactor conditional to alert player for no piece at given location

--- lib/chess.rb
+++ lib/chess.rb
@@ -29,10 +29,14 @@ class Chess
       input = gets.chomp.delete(' ')
       # This conditional checks whether the given input is valid && there is a piece present at the given
       # location
-      if valid_location?(input) && board[input] != " "
+      case
+      when valid_location?(input) && board[input] == " "
+        print "The location you entered has no chess piece."
+        next
+      when valid_location?(input) && board[input] != " "
         chosen_piece = board[input]
         player.chosen_piece = chosen_piece
-        get_piece_placement(chosen_piece)
+        get_piece_placement(player)
         break
       else
         next
@@ -41,14 +45,13 @@ class Chess
   end
 
   def display_valid_input_format
-    puts "\n When choosing which piece to move or where to put said piece",
+    puts "\nWhen choosing which piece to move or moving the piece",
          "the only valid input you can give has to be like:",
          "1a",
          "Where '1' is the row and 'a' is the column of the board.",
-         "Remember your row cannot be greater than 8 and your column cannot be any
-         alphabet higher than 'h'."
+         "\033[31mRemember your row cannot be greater than 8 and your column cannot be any alphabet higher than 'h'.\033[0m"
   end
 
-  def get_piece_placement(chosen_piece)
+  def get_piece_placement(player)
   end
 end
