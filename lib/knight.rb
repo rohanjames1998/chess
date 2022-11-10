1fa6ad418e46c57238e6759091dd1186b5fb304c
Rohan James <rohanjames9282@gmail.com>
2022-11-10 16:33:34 +0530

    Add movement methods for rook

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,4 +1,4 @@
-
+require_relative 'board'
 class Knight
 
 attr_reader :unicode, :color
@@ -18,4 +18,42 @@ attr_reader :unicode, :color
     @color = 'black'
   end
 
+  def generate_possible_moves(initial_loc, board)
+    # Since this method is hard coded and most of its work is done
+    # by #check_valid_move we are not going to test this method.
+    row = initial_loc[0].to_i
+    col = initial_loc[1].ord
+    all_moves = []
+    all_moves << (row + 2).to_s + (col - 1).chr #up-up-left
+    all_moves << (row + 2).to_s + (col + 1).chr #up-up-right
+    all_moves << (row - 2).to_s + (col - 1).chr #down-down-left
+    all_moves << (row - 2).to_s + (col + 1).chr #down-down-right
+    all_moves << (row + 1).to_s + (col + 2).chr #right-right-up
+    all_moves << (row + 1).to_s + (col - 2).chr #left-left-up
+    all_moves << (row - 1).to_s + (col + 2).chr #right-right-down
+    all_moves << (row - 1).to_s + (col - 2).chr #left-left-down
+
+    possible_moves = check_valid_moves(all_moves, board)
+    possible_moves
+  end
+
+  def check_valid_moves(all_moves, board)
+    valid_moves = []
+    all_moves.each do |move|
+      piece = board[move]
+      case
+      when piece == nil # For moves outside the board
+        next
+      when piece == "" # For empty places
+        valid_moves << move
+        next
+      when piece.color != color # For enemy pieces
+        valid_moves << move
+        next
+      else # For ally pieces
+        next
+      end
+    end
+    valid_moves
+  end
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -8,10 +8,11 @@ describe Knight do
 
   before do
     allow(knight).to receive(:color).and_return('white')
+  end
 
   describe '#check_valid_moves' do
     context 'When all eight possible moves are possible' do
-      xit "returns all 8 possible moves" do
+      it "returns all 8 possible moves" do
         all_moves = ['6e', '6c', '2e', '2c', '3f', '5f', '3b', '5b']
         allow(board).to receive(:[]).and_return('')
         possible_moves = knight.check_valid_moves(all_moves, board)
@@ -19,21 +20,30 @@ describe Knight do
       end
     end
     context "When there is an ally piece that blocks a move" do
-      xit "returns every move except the one blocked ally piece" do
+      it "returns every valid move except the one blocked by ally piece" do
         all_moves = ['6e', '6c', '2e', '2c', '3f', '5f', '3b', '5b']
-        allow(board).to receive(:[]).and_return(dummy_ele, '')
-        allow(dummy_ele).to receive(:color).and_return('white')
+        allow(board).to receive(:[]).and_return(dummy_piece, '')
+        allow(dummy_piece).to receive(:color).and_return('white')
         possible_moves = knight.check_valid_moves(all_moves, board)
-        expect(possible_moves).not_to include('6e')
+        expect(possible_moves).not_to include("6e")
       end
     end
     context "When there is an enemy piece knight can kill" do
-      xit "returns enemy's location as one of the possible move" do
+      it "returns enemy's location as one of the possible move" do
         all_moves = ['6e', '6c', '2e', '2c', '3f', '5f', '3b', '5b']
-        allow(board).to_receive(:[]).and_return(dummy_ele, '')
-        allow(dummy_ele).to receive(:color).and_return('black')
+        allow(board).to receive(:[]).and_return(dummy_piece, '')
+        allow(dummy_piece).to receive(:color).and_return('black')
         possible_moves = knight.check_valid_moves(all_moves, board)
         expect(possible_moves).to include('6e')
+      end
+    end
+    context "When one of the moves is invalid (i.e., outside the board)" do
+      it "returns all moves except invalid" do
+        invalid_move = '0e'
+        all_moves = [invalid_move, '6c', '2e', '2c', '3f', '5f', '3b', '5b']
+        allow(board).to receive(:[]).and_return(nil, '')
+        possible_moves = knight.check_valid_moves(all_moves, board)
+        expect(possible_moves).not_to include(invalid_move)
       end
     end
   end
