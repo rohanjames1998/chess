52fa0ee103ce5b02c3322ac41927258f7542b0e1
Rohan James <rohanjames9282@gmail.com>
2022-11-11 16:39:49 +0530

    Add logic to make sure player move only their color

--- lib/chess.rb
+++ lib/chess.rb
@@ -11,10 +11,10 @@ class Chess
     @board = Board.new
   end
 
-  def valid_location?(coordinates)
+  def valid_input?(coordinates, player)
     row = coordinates[0]
     col = coordinates[1].downcase
-    if /[1-8]$/.match(row) && /[a-h]$/.match(col)
+    if /[1-8]$/.match(row) && /[a-h]$/.match(col) && board[coordinates].color == player.color
       return true
     end
     display_valid_input_format
@@ -26,17 +26,15 @@ class Chess
   def get_player_piece(player)
     loop do
       puts "Which piece do you want to move?:"
-      input = gets.chomp.delete(' ')
+      input = gets.chomp.downcase.delete(' ')
       # This conditional checks whether the given input is valid && there is a piece present at the given
       # location
       case
-      when valid_location?(input) && board[input] == " "
+      when valid_input?(input, player) && board[input] == " "
         print "The location you entered has no chess piece."
         next
-      when valid_location?(input) && board[input] != " "
-        chosen_piece = board[input]
-        player.chosen_piece = chosen_piece
-        get_piece_placement(player)
+      when valid_input?(input, player) && board[input] != " "
+        move_piece(player, board, player)
         break
       else
         next
@@ -44,14 +42,14 @@ class Chess
     end
   end
 
+  def move_piece(location, board, player)
+  end
+
   def display_valid_input_format
-    puts "\nWhen choosing which piece to move or moving the piece",
+    puts "\nYou either cannot move that piece or your input is invalid.",
          "the only valid input you can give has to be like:",
          "1a",
          "Where '1' is the row and 'a' is the column of the board.",
          "\033[31mRemember your row cannot be greater than 8 and your column cannot be any alphabet higher than 'h'.\033[0m"
-  end
-
-  def get_piece_placement(player)
   end
 end
