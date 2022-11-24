0748928bc8a525232dba4e0d96b1e1f0d0302c9a
Rohan James <rohanjames9282@gmail.com>
2022-11-24 17:36:55 +0530

    Add #clear? to check if a move is clear for king

--- lib/king.rb
+++ lib/king.rb
@@ -20,7 +20,7 @@ class King
   end
 
   def generate_potential_moves(initial_loc, board)
-    all_moves = generate_king_moves(initial_loc)
+    all_moves = generate_king_moves(initial_loc, board)
     potential_moves = check_valid_moves(all_moves, board)
     potential_moves
   end
@@ -28,12 +28,30 @@ class King
   def check_valid_moves(all_moves, board)
     potential_moves = []
     all_moves.each do |move|
-      potential_moves << move if clear?(move)
+      potential_moves << move if clear?(move, board)
     end
     potential_moves
   end
 
-  def clear?(move)
+  def clear?(move, board)
+    # This method checks if the location is threatened by an enemy piece
+    # Returns true if there is no threat; returns false if there is a threat.
+    case
+    when diagonal_threat?(move, board)
+      return false
+    when vertical_threat?(move, board)
+      return false
+    when horizontal_threat?(move, board)
+      return false
+    when knight_threat?(move, board)
+      return false
+    when king_threat?(move, board, color)
+      return false
+    when pawn_threat?(move, board)
+      return false
+    else
+      return true
+    end
   end
 
 end
