414bbc3f60b2a3fcb5c130d094d897778353e0d8
Rohan James <rohanjames9282@gmail.com>
2022-12-02 11:53:03 +0530

    Fix #pawn_threat? always returning false

--- lib/king.rb
+++ lib/king.rb
@@ -75,9 +75,13 @@ class King
   end
 
 
-  # In the threat detection methods (#diagonal_threat?, #horizontal_threat?, and so on) we first generate all the moves from the possible move location and then check if our king can safely move to this location.
+  # In the threat detection methods (#diagonal_threat?, #horizontal_threat?, and so on)
+  # we first generate all the moves from the possible move location and then check if our
+  #king can safely move to this location.
   def diagonal_threat?(move, board)
-    # Since these movement methods will return enemy pieces (if any) as their last element (they always return arrays) all we will do is check if the last element is a bishop or queen (these method doesn't return ally pieces).
+    # Since these movement methods will return enemy pieces (if any) as their last element
+    #(they always return arrays) all we will do is check if the last element is a bishop or queen
+    #(these method doesn't return ally pieces).
     last_top_right = generate_top_right_moves(move, board, color).last
     last_top_left = generate_top_left_moves(move, board, color).last
     last_down_right = generate_down_right_moves(move, board, color).last
@@ -159,13 +163,13 @@ class King
   def pawn_threat?(move, board, color)
     # Possible enemy pawn locations differ according to king's color.
     row = move[0].to_i
-    col = move[1].ord - 97
+    col = move[1].ord
     if color == 'white'
-      loc_1 = (row + 1) + (col - 1) #up-left
-      loc_2 = (row + 1) + (col + 1) #up-right
+      loc_1 = (row + 1).to_s + (col - 1).chr #up-left
+      loc_2 = (row + 1).to_s + (col + 1).chr #up-right
     else # For black
-      loc_1 = (row - 1) + (col - 1) #down-left
-      loc_2 = (row - 1) + (col + 1) #down-right
+      loc_1 = (row - 1).to_s  + (col - 1).chr #down-left
+      loc_2 = (row - 1).to_s + (col + 1).chr #down-right
     end
     # checking for enemy pawn on both locations
     case
@@ -179,7 +183,8 @@ class King
   end
 
   def enemy_pawn?(location, board, color)
-    if board[location].is_a?(Pawn) && board[location].color != color
+    piece = board[location]
+    if piece.is_a?(Pawn) && piece.color != color
       return true
     end
     return false
