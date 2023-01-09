f79575b680a470af22ee84313e174f5575e30bac
Rohan James <rohanjames9282@gmail.com>
2023-01-09 18:13:59 +0530

    Add tests for #white and #black

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -6,17 +6,36 @@ describe Knight do
   let(:board) { instance_double(Board) }
   let(:dummy_piece) { instance_double(Pawn) }
 
-  before do
-    allow(knight).to receive(:color).and_return('white')
+  describe '#white' do
+    context 'When called' do
+      it 'makes knight white' do
+        knight.white
+        expect(knight.color).to eq('white')
+        expect(knight.unicode).to eq("\u2658")
+      end
+    end
+  end
+
+  describe '#black' do
+    context 'When called' do
+      it 'makes knight black' do
+        knight.black
+        expect(knight.color).to eq('black')
+        expect(knight.unicode).to eq("\u265e")
+      end
+    end
   end
 
   describe '#check_valid_moves' do
-    context 'When all eight possible moves are possible' do
-      it "returns all 8 possible moves" do
+    before do
+      allow(knight).to receive(:color).and_return('white')
+    end
+    context 'When all eight potential moves are potential' do
+      it "returns all 8 potential moves" do
         all_moves = ['6e', '6c', '2e', '2c', '3f', '5f', '3b', '5b']
         allow(board).to receive(:[]).and_return('')
-        possible_moves = knight.check_valid_moves(all_moves, board)
-        expect(possible_moves).to eq(all_moves)
+        potential_moves = knight.check_valid_moves(all_moves, board)
+        expect(potential_moves).to eq(all_moves)
       end
     end
     context "When there is an ally piece that blocks a move" do
@@ -24,26 +43,26 @@ describe Knight do
         all_moves = ['6e', '6c', '2e', '2c', '3f', '5f', '3b', '5b']
         allow(board).to receive(:[]).and_return(dummy_piece, '')
         allow(dummy_piece).to receive(:color).and_return('white')
-        possible_moves = knight.check_valid_moves(all_moves, board)
-        expect(possible_moves).not_to include("6e")
+        potential_moves = knight.check_valid_moves(all_moves, board)
+        expect(potential_moves).not_to include("6e")
       end
     end
     context "When there is an enemy piece knight can kill" do
-      it "returns enemy's location as one of the possible move" do
+      it "returns enemy's location as one of the potential move" do
         all_moves = ['6e', '6c', '2e', '2c', '3f', '5f', '3b', '5b']
         allow(board).to receive(:[]).and_return(dummy_piece, '')
         allow(dummy_piece).to receive(:color).and_return('black')
-        possible_moves = knight.check_valid_moves(all_moves, board)
-        expect(possible_moves).to include('6e')
+        potential_moves = knight.check_valid_moves(all_moves, board)
+        expect(potential_moves).to include('6e')
       end
     end
     context "When one of the moves is invalid (i.e., outside the board)" do
-      it "returns all moves except invalid" do
+      it "returns all moves except invalid move" do
         invalid_move = '0e'
         all_moves = [invalid_move, '6c', '2e', '2c', '3f', '5f', '3b', '5b']
         allow(board).to receive(:[]).and_return(nil, '')
-        possible_moves = knight.check_valid_moves(all_moves, board)
-        expect(possible_moves).not_to include(invalid_move)
+        potential_moves = knight.check_valid_moves(all_moves, board)
+        expect(potential_moves).not_to include(invalid_move)
       end
     end
   end
