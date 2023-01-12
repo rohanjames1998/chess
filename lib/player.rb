37e61abbe9ac9e730e17960580c94ad555eed4ad
Rohan James <rohanjames9282@gmail.com>
2023-01-12 12:03:35 +0530

    Fix player's name appearing blank

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,38 @@
 
 class Player
 
-  attr_accessor :name, :color, :chosen_piece
+  attr_accessor :name, :color, :king_loc
 
   def initialize
     @name = ''
     @color = ''
-    @chosen_piece = ''
+    @king_loc = ''
+  end
+
+  def white
+    @name = get_name
+    @color = 'white'
+    @king_loc = '1e'
+  end
+
+  def black
+    @name = get_name
+    @color = 'black'
+    @king_loc = '8e'
+  end
+
+
+  def get_name
+    puts "Enter your name:"
+    loop do
+      name = gets.chomp.capitalize.strip
+      if name.length > 10
+        puts "Please enter a smaller name."
+        next
+      else
+        return name
+        break
+      end
+    end
   end
 end
